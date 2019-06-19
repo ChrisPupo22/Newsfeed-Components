b2549f9128b5127878bbe2e85599c0930a290494
Christopher Pupo <chrispupo@Christophers-iMac.local>
2019-06-19 19:13:36 -0400

    final push of the day

--- Article/Article.js
+++ Article/Article.js
@@ -3,18 +3,25 @@
 class Article {
   constructor(domElement) {
     // assign this.domElement to the passed in domElement
-    this.domElement;
+    this.domElement = domElement;
     // create a reference to the ".expandButton" class. 
-    this.expandButton;
+    this.expandButton = this.domElement.querySelector('.expandButton')
     // Using your expandButton reference, update the text on your expandButton to say "expand"
-    
+    this.expandButton.textContent = 'expand'
+    this.expandButton.style.border = '1px solid black'
+    this.expandButton.style.padding = '7px'
+    this.expandButton.style.borderRadius = '7.5px'
+    this.expandButton.style.background = '#388E3C'
     // Set a click handler on the expandButton reference, calling the expandArticle method.
-    
+    domElement.addEventListener('click', this.expandArticle);
+    console.log(domElement.expandButton);
   }
+  
 
   expandArticle() {
     // Using our reference to the domElement, toggle a class to expand or hide the article.
-
+    event.currentTarget.classList.toggle('article');
+  
   }
 }
 
@@ -26,4 +33,7 @@ class Article {
 
 */
 
-let articles;
+let articles = document.querySelectorAll('.article');
+
+articles = Array.from(articles).forEach(article => { new Article(article)});
+
